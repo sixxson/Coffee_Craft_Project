fa96df392eadea36d83c84a72e0e64f0dc6eb8a0
minh le <117999133+sixxson@users.noreply.github.com>
2025-05-03 01:21:29 +0700

    update services

--- coffee-craft-ser/src/index.ts
+++ coffee-craft-ser/src/index.ts
@@ -36,9 +36,7 @@ app.use(
   cors({
     origin: [
       "https://coffee-craft-admin-portal.vercel.app",
-      "https://coffee-craft.vercel.app",
       "https://coffee-craft-project-git-main-sixxsons-projects.vercel.app",
-      "http://localhost:3000",
     ],
     credentials: true, // Allow cookies to be sent with requests
   })
